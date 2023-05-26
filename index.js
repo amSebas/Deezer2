5f65da85cef8c8687c2908ff724ff20b6b74f79f
amSebas <sebaslgr@gmail.com>
2023-05-26 00:56:23 -0500

    la cague y me toco crear otro repositorio

---
+++ index.js
@@ -0,0 +1,21 @@
+//const menu2 = document.getElementById('oferta-2')
+//const menu3 = document.getElementById('oferta-3')
+//const botonDespliegue = document.getElementById('boton-despliegue')
+//const botonDespliegue2 = document.getElementById('boton-despliegue-2')
+
+function cargarPagina(){
+    menu2.style.display = 'none'
+    menu3.style.display = 'none'
+}
+
+function despliegueAnual(){
+    menu2.style.display = 'flex'
+    botonDespliegue.style.display = 'none'
+}
+
+function despliegueEstudiante(){
+    menu3.style.display = 'flex'
+    botonDespliegue2.style.display = 'none'
+}
+
+window.addEventListener("load", cargarPagina())
